0599cbd4b6371ab381d24834693815a70eef44f1
gladstone28 <gladstonerose@hotmail.com>
2022-04-23 18:44:22 -0500

    updated script.py

--- script.py
+++ script.py
@@ -33,7 +33,41 @@ def get_input():
       if user_input in choices:
         for i in range(len(stacks)):
          return stacks[i]
-
         
 #Play the Game
+num_user_moves = 0
+while right_stack.get_size() != num_disks:
+  print("\n\n\n...Current Stacks...")
+  for stack in stacks:
+    stack.print_items()
+  while True:
+    print("\nWhich stacks do you want to remove?\n")
+    from_stack = get_input()
+    print("\nWhich stacks do you want to move to?\n")
+    to_stack = get_input()
+    if from_stack.get_size() ==0:
+      print("\n\nInvalid move. Try again")
+    elif to_stack.get_size()==0 or 
+  
+  from_stack.peek() < to_stack.peek():
+      disk = from_stack.pop()
+      to_stack.push(disk)
+      num_user_moves += 1
+      break
+    else:
+      print("\n\nInvalid move. Try again")
+
+  print("\n\nYou completed the game in {0} moves, and the optimal number of moves is {1}".format(num_user_moves, num_optimal_moves))
+  
+
+
+
+
+
+  
+
+
+
+
+
 
